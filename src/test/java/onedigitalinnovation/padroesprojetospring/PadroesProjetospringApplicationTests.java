c9c177571fc2a056ffa806a1127071595c46c05a
alex-almeida12 <alex.almeida12@gmail.com>
2022-06-23 17:38:30 -0300

    first commit

---
+++ src/test/java/onedigitalinnovation/padroesprojetospring/PadroesProjetospringApplicationTests.java
@@ -0,0 +1,13 @@
+package onedigitalinnovation.padroesprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
